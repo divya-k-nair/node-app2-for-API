5262b03133aa0663e7bca32f408ce00a42a7c269
divya-k-nair <divyakaladharannair@gmail.com>
2021-10-22 14:52:18 +0530

    project completed

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
  
 
 app.use(bodyParser.json())
+app.get('/read',(req,res)=>{
+    res.send("web app development")
+})
 app.post('/div',(req,res)=>{
     var getNum1=parseFloat(req.body.num1)
     var getNum2=parseFloat(req.body.num2)
     var result=(getNum1/getNum2)
     res.json({"result":result})
 })
-app.listen(3000,(req,res)=>{
+app.listen(process.env.PORT||3000,(req,res)=>{
     console.log("server started at http://localhost:3000")
 })
